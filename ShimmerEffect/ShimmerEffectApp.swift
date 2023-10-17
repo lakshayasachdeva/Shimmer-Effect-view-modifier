3ec7cce5769ceb6ae0e82499a2f855ae822375d8
Lakshaya Sachdeva <lakshaya.sachdeva@colearn.id>
2023-10-17 12:35:45 +0530

    Initial Commit

---
+++ ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by Lakshaya Sachdeva on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
